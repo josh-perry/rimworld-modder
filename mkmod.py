f048b44fb49f50ac6c3d8d1f406f385e74695ad7
Josh Perry <joshperry@outlook.com>
2016-08-18 15:51:44 +0100

    Added some About.xml writing. Adding reading version from Version.txt.

--- mkmod.py
+++ mkmod.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+import re
+
 
 directories = ["About",
                "Assemblies",
@@ -12,6 +14,15 @@ directories = ["About",
                "Source",
                "Textures"]
 
+about_template = '''<?xml version="1.0" encoding="utf-8"?>
+<ModMetaData>
+    <name>{Name}</name>
+    <author>{Author}</author>
+    <url>{Url}</url>
+    <targetVersion>{Version}</targetVersion>
+    <description>{Description}</description>
+</ModMetaData>'''
+
 
 def create_dirs(mods_path, mod_name):
     mod_dir = os.path.join(mods_path, mod_name)
@@ -23,7 +34,31 @@ def create_dirs(mods_path, mod_name):
     for d in directories:
         os.mkdir(os.path.join(mod_dir, d))
 
-    open(os.path.join(mod_dir, "About/About.xml"), 'a').close()
+    with open(os.path.join(mod_dir, "About/About.xml"), 'w') as f:
+        data = {"Name": mod_name,
+                "Author": "",
+                "Url": "",
+                "Version": get_version(mods_path),
+                "Description": ""}
+
+        about_xml_content = about_template.format(**data)
+        f.write(about_xml_content)
+
+
+def get_version(mods_path):
+    rimworld_dir = os.path.join(mods_path, "..")
+
+    try:
+        with open(os.path.join(rimworld_dir, "Version.txt")) as f:
+            # version = f.read().replace("\n", "")
+            # return version.strip()
+
+            contents = f.read()
+            m = re.search(r'\d+\.\d+\.\d+', contents)
+            return m.group(0)
+    except Exception as ex:
+        print(ex)
+        return ""
 
 
 def main():
